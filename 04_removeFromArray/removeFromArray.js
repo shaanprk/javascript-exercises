5ad892814708a93a8a233759962e7c5512a0410e
Sean <jungbeanshaanprk@gmail.com>
2024-09-16 05:22:16 +0900

    Finished 01-03

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] == element) {
+            array.splice(i, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
