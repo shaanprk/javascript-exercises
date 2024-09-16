1f927a780d4cad90ba7a818740bacddd479f497c
Sean <jungbeanshaanprk@gmail.com>
2024-09-16 23:34:51 +0900

    Finished 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.replace(/[!,\s.]/g,'').toLowerCase();
+    console.log(string);
+    let revStr = string.split("").reverse().join("");
+    if (string == revStr) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
