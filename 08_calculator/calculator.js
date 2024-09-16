ca30f0ac2b7d8a5bc646cbdbfb895224691a6dce
Sean <jungbeanshaanprk@gmail.com>
2024-09-16 23:19:38 +0900

    Finished 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let output = 0;
+  for (let i = 0; i < arr.length; i++) {
+    output += arr[i];
+  }
+  return output;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let output = 1;
+  for (let i = 0; i < arr.length; i++) {
+    output *= arr[i];
+  }
+  return output;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let output = 1;
+  for (let i = num; num > 0; num--) {
+    output *= num;
+  }
+  return output;
 };
 
 // Do not edit below this line
